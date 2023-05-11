54643b6ff00fce798bce96c1d5bebcd8d5f21f27
NadindraDwiA <nadindra.dwi.2005357@students.um.ac.id>
2023-05-11 19:54:00 +0700

    home_activity

---
+++ app/src/main/java/id/com/wanderwisely/HomeActivity.kt
@@ -0,0 +1,11 @@
+package id.com.wanderwisely
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
