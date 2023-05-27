598295dd468277c60138cc573619a422df6ad48c
RulH19 <nurulh298hd@gmail.com>
2023-05-27 10:46:25 +0700

    add pager + maps

--- app/src/main/java/id/com/wanderwisely/ui/detail/DetailActivity.kt
+++ app/src/main/java/id/com/wanderwisely/ui/detail/DetailActivity.kt
@@ -2,7 +2,13 @@ package id.com.wanderwisely.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import id.com.wanderwisely.R
 import id.com.wanderwisely.databinding.ActivityDetailBinding
+import id.com.wanderwisely.ui.detail.adapter.SectionsPagerAdapter
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding : ActivityDetailBinding
@@ -10,5 +16,23 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //section adapter
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tab_layout)
+        TabLayoutMediator(tabs, viewPager){ tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+        supportActionBar?.elevation = 0f
+    }
+
+    companion object{
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2
+        )
     }
 }
--- app/src/main/java/id/com/wanderwisely/ui/detail/frament/DetailFragment.kt
+++ app/src/main/java/id/com/wanderwisely/ui/detail/frament/DetailFragment.kt
@@ -1,26 +1,47 @@
 package id.com.wanderwisely.ui.detail.frament
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
+
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import id.com.wanderwisely.R
-
+import id.com.wanderwisely.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+    private var _binding : FragmentDetailBinding? = null
+    private val binding get() = _binding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val callback = OnMapReadyCallback { googleMap ->
+        val location = LatLng(-5.110833, 127.940833)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title("Gunung Rinjani")
+                .snippet("Gunung Rinjani Jawa timur")
+        )
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
 
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
 }
