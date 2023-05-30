81dcf9f530a124410250aef6f42f84c01f40fc37
NadindraDwiA <nadindra.dwi.2005357@students.um.ac.id>
2023-05-30 10:45:23 +0700

    Onboarding Animation

--- app/src/main/java/id/com/wanderwisely/ui/survey/SurveyActivity.kt
+++ app/src/main/java/id/com/wanderwisely/ui/survey/SurveyActivity.kt
@@ -1,12 +1,22 @@
 package id.com.wanderwisely.ui.survey
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import id.com.wanderwisely.R
+import id.com.wanderwisely.databinding.ActivitySurveyBinding
+import id.com.wanderwisely.ui.detail.DetailActivity
 
 class SurveyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySurveyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_survey)
+        binding = ActivitySurveyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnContinue.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
